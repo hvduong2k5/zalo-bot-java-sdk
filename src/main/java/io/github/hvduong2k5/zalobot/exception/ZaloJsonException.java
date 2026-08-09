2640f71fe77960e08b1ae356a3f7c84da80597e2
hvduong <hd2005555@gmail.com>
2026-08-09 17:29:32 +0700

    fix: validate JacksonAdapter inputs and introduce ZaloJsonException

---
+++ src/main/java/io/github/hvduong2k5/zalobot/exception/ZaloJsonException.java
@@ -0,0 +1,14 @@
+package io.github.hvduong2k5.zalobot.exception;
+
+/**
+ * Exception thrown when there is an error serializing or deserializing JSON.
+ * <p>
+ * This exception wraps the underlying JSON processing exceptions (e.g., Jackson's JsonProcessingException)
+ * to hide the implementation details from the SDK users.
+ */
+public class ZaloJsonException extends RuntimeException {
+
+    public ZaloJsonException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
